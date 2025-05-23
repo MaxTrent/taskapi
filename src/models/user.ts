dbe909edabd16a007b56e17141709bd8bc8e4bbb
Samuel <samueladelowo92@gmail.com>
2025-05-24 00:09:33 +0100

    env sample

---
+++ src/models/user.ts
@@ -0,0 +1,53 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+
+interface UserAttributes {
+  id: number;
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+class User extends Model<UserAttributes> implements UserAttributes {
+  public id!: number;
+  public email!: string;
+  public password!: string;
+  public role!: 'user' | 'admin';
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+const configureUserModel = (sequelize: Sequelize) => {
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM('user', 'admin'),
+        defaultValue: 'user',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users',
+    }
+  );
+  return User;
+};
+
+export { User, configureUserModel, UserAttributes };
